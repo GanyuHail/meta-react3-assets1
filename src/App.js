315a2c45ff2ff71324bf703840632ad9f484a617
GanyuHail <103945576+GanyuHail@users.noreply.github.com>
2023-10-31 17:29:34 +0000

    img tag fixed spread open close </>

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ function App() {
   return (
     <div className="App">
       <h1>Task: Add an image below</h1>
-      <img>
+      <img
       height = {500}
       src = {require ("./assets/logo.png")}
       alt = "Logo"
-      </img>
+      />
     </div>
   );
 }
